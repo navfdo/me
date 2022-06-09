c92b07a1df2ec63a75b755b1a8e5b717b520cd60
Naveen <navfdo1@gmail.com>
2022-06-09 16:56:06 +1000

    done exercises

--- set2/exercise0.py
+++ set2/exercise0.py
@@ -17,6 +17,9 @@ obvious answer, that's because it is!
 """
 
 
+from re import S
+
+
 def add_1(a_number):
     """Return a number that is 1 bigger than number given.
 
@@ -66,7 +69,7 @@ def shout(a_string):
           they all do different things. You'll need to actually read the
           docs to find out which one you actually need.
     """
-    the_answer = None
+    the_answer = str.upper(a_string)
     return the_answer
 
 
@@ -79,7 +82,7 @@ def really_shout(a_string):
     that you've reused the function you already wrote.
     Look up how to 'concatinate' strings to make this happen.
     """
-    the_answer = None
+    the_answer = str.upper(a_string) + "!"
     return the_answer
 
 
@@ -95,7 +98,7 @@ def shout_with_a_number(a_string, a_number):
           challenge, see if you can make the test pass with at least two ways
           of doing the same job.
     """
-    the_answer = None
+    the_answer = a_string.upper() + " " + str(a_number)
     return the_answer
 
 
